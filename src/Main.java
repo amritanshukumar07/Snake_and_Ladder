5490dcb34b1d99366448fd9c5c410947ff54acbe
Amritanshu Kumar <amritanshu8271@gmail.com>
2024-12-16 16:50:42 +0530

    Process repeated till plater reaches 100

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder game");
         System.out.println("Player 1 start at 0");
-
-        int dice= (int) (Math.random()*6)+1;
-
-        System.out.println("Value of dice is "+ dice);
         int player1Pos=0;
-        int option= (int)(Math.random()*3)+1;
-        // System.out.println(option);
-        switch (option){
-            case 1 -> player1Pos=player1Pos;
-            case 2 -> player1Pos+=dice;
-            case 3 -> player1Pos-=dice;
-        }
-        if(player1Pos <0){
-            player1Pos=0;
-        }
-        System.out.println("Player position is "+ player1Pos);
+-        while(player1Pos<100) {
+            int dice = (int) (Math.random() * 6) + 1;
 
+            System.out.println("Value of dice is " + dice);
 
+            int option = (int) (Math.random() * 3) + 1;
+            // System.out.println(option);
+            switch (option) {
+                case 1 -> player1Pos = player1Pos;
+                case 2 -> player1Pos += dice;
+                case 3 -> player1Pos -= dice;
+            }
+            if (player1Pos < 0) {
+                player1Pos = 0;
+            }
+            System.out.println("Player position is " + player1Pos);
+             it++;
+        }
 
     }
 }
