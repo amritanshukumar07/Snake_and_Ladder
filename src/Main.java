3b53254a722e753949e9db43827225c44b5219eb
Amritanshu Kumar <amritanshu8271@gmail.com>
2024-12-16 16:24:28 +0530

    Player position added

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,18 @@ public class Main {
         int dice= (int) (Math.random()*6)+1;
 
         System.out.println("Value of dice is "+ dice);
-
+        int player1Pos=0;
+        int option= (int)(Math.random()*3)+1;
+        // System.out.println(option);
+        switch (option){
+            case 1 -> player1Pos=player1Pos;
+            case 2 -> player1Pos+=dice;
+            case 3 -> player1Pos-=dice;
+        }
+        if(player1Pos <0){
+            player1Pos=0;
+        }
+        System.out.println("Player position is "+ player1Pos);
 
 
 
